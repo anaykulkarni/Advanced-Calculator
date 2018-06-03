6ac9e01a30b7bb73c673812f6c5b0334611b21ff
Anay Kulkarni <38681469+anaykulkarni@users.noreply.github.com>
2018-06-03 21:51:24 +0530

    Added ArithmeticModule class for the scientific calculator along with 50 JUnit Test Cases

---
+++ Advanced Calculator/src/scientificcalculator/ArithmeticModule.java
@@ -0,0 +1,176 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scientificcalculator;
+
+/**
+ * The class ArithmeticModule performs the arithmetic calculations of the scientific calculator
+ * such as squaring, cubing, finding roots, factorial, powers, negation, inversion, etc.
+ * 
+ * @version     1.0
+ * @author      Anay Kulkarni.
+ */
+public class ArithmeticModule {    
+    /**
+     * This method simply returns the square of the number x.
+     * @param x The number whose square is required.
+     * @return The square of x.
+     */
+    public double xSquare(double x){
+        return (x*x);
+    }
+    
+    /**
+     * This method simply returns the cube of the number x.
+     * @param x The number whose cube is required.
+     * @return The cube of x.
+     */
+    public double xCube(double x){
+        return (x*x*x);
+    }
+    
+    /**
+     * This method simply returns the inverse of the number x.
+     * @param x The number whose cube is required.
+     * @return The cube of x.
+     */
+    public double xInverse(double x){
+        return (1.0/x);
+    }
+    
+    /**
+     * This method returns the negated number.
+     * @param x number to be negated.
+     * @return negation of x.
+     */
+    public double negate(double x){
+        return (-1.0*x);
+    }
+    
+    /**
+     * This method calculates the result of x to the power y.
+     * @param x variable for base value.
+     * @param y variable for index value.
+     * @return returns the value x to the power( x^y )
+     */
+    public double xPowerY(double x, double y){
+        return Math.pow(x, y);
+    }
+    
+    /**
+     * This method return the yth root of x.
+     * The root returned is the principal branch of all roots. For instance, a square root has two roots,
+     * cube root has three, fourth root has four and so on. This method returns the principal branch only.
+     * even roots of negative numbers produce complex numbers and hence the output will be of the form
+     * <b>a + ib</b>. There are nine possible cases in total for the positive, negative and zero values of x and y.
+     * The <b>special cases</b> would be,
+     * <ul>
+     * <li> when x=0 and y=0, result is undefined(NaN). </li>
+     * <li> when x=0 and y&gt;0, result is zero. </li>
+     * <li> when x=0 and y&lt;0, result is infinity. </li>
+     * <li> when x&gt;0 and y=0, result is infinity. </li>
+     * <li> when x&lt;0 and y=0, result is undefined(NaN). </li>
+     * </ul>
+     * @param x variable for base
+     * @param y variable for root
+     * @return the yth root of x in form of a complex number.
+     */
+    public String yRootX(double x, double y){
+        
+        if(x==0){
+            if(y==0){
+                return "NaN";                                                   //when x=0 and y=0
+            } else if(y<0){
+                return "infinity";                                              //when x=0 and y<0
+            } else if(y>0){
+                return "0";                                                     //when x=0 and y>0
+            }
+        } 
+        else if(x > 0){
+            if(y!=0)
+                return Double.toString(Math.pow(x, (1.0/y)));                   //when x>0 and y not 0
+            else 
+                return "infinity";                                              //when x> 0 and y=0    
+        }        
+        else if(x < 0){                                                         
+            if(y == 0){
+                return "NaN";                                                   //since limit does not exist for negatives raised infinity. 
+                                                                                //Result oscillates between -infinity and +infinity  
+            }
+            else if(y>0){
+                double real;
+                double imag;
+                if(y % 2.0 == 0){
+                    real = Math.pow( Math.abs(x), (1.0/y))*Math.cos(Math.PI/y); //even roots of negative numbers produce complex numbers.
+                    imag = Math.pow( Math.abs(x), (1.0/y))*Math.sin(Math.PI/y); //calculated using Euler's formula and De'Moivre's theorem.
+                                                                                //returns only the principal branch of roots.
+                    
+                    if(imag>0)                                                  
+                        return String.format("%.6f + i%.6f", real, imag);
+                    else if(imag<0)
+                        return String.format("%.6f - i%.6f", real, Math.abs(imag));
+                    else
+                        return String.format("%.6f", real);
+                }
+                else{
+                    return Double.toString(-1*Math.pow(Math.abs(x), (1.0/y)));  //principal branch of odd roots is a real number.
+                }
+            }else {
+                y = Math.abs(y);
+                double real;
+                double imag;
+                if(y % 2.0 == 0){
+                    real = Math.pow( Math.abs(x), (1.0/y))*Math.cos(Math.PI/y); //even roots of negative numbers produce complex numbers.
+                    imag = Math.pow( Math.abs(x), (1.0/y))*Math.sin(Math.PI/y); //calculated using Euler's formula and De'Moivre's theorem.
+                                                                                //returns only the principal branch of roots.
+                    
+                    /* negative root of a number is equivalent to reciprocal of the positive root. Since, here the root is even
+                     we perform inverse of complex number using the conjugate.
+                    */ 
+                    double productOfConjugates = ((real*real)+(imag*imag));
+                    real = real/productOfConjugates;
+                    imag = (-imag)/productOfConjugates;
+                    
+                    if(imag>0)                                                  
+                        return String.format("%.6f + i%.6f", real, imag);
+                    else if(imag<0)
+                        return String.format("%.6f - i%.6f", real, Math.abs(imag));
+                    else
+                        return String.format("%.6f", real);
+                }
+                else{
+                    return Double.toString(this.xInverse( -1*Math.pow(Math.abs(x), (1.0/y)) )); //principal branch of odd roots is a real number.
+                }
+            }
+        }
+        return "Error. Something went wrong";                                                         
+    }
+    
+    /**
+     * This method returns the square root of the number.
+     * This method only works for non-negative inputs. To obtain square roots of negative numbers
+     * use the yRootX() method as yRootX(x, 2). This will produce a complex number as output.
+     * @param x The number whose square root is required.
+     * @return the square root of x.
+     */
+    public Double squareRoot(double x) {
+            return Math.sqrt(x);
+    }
+    
+    /**
+     * This method returns the factorial of a number.
+     * @param n number whose factorial is to be computed.
+     * @return factorial of n
+     */
+    public long nFactorial(int n){
+        long fact=1;
+        if(n==0 || n==1)
+            return 1;
+        for(int i=n ; i>1; i--)
+            fact *= i;
+        return fact;
+    }
+
+}
---
+++ Advanced Calculator/test/scientificcalculator/ArithmeticModuleTest.java
@@ -0,0 +1,294 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scientificcalculator;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * JUnit Test class for the Arithmetic module.
+ * 
+ * @version     1.0
+ * @author      Anay Kulkarni
+ */
+public class ArithmeticModuleTest {
+    
+    ArithmeticModule am;
+    double maxPrecision = 0.00000001;
+    
+    public ArithmeticModuleTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        am = new ArithmeticModule();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void squareRootTest1() {
+        assertEquals( 2.0, am.squareRoot( 4.0 ), maxPrecision);
+    }
+    
+    @Test
+    public void squareRootTest2() {
+        assertEquals( 49.12687655, am.squareRoot( 2413.45 ), maxPrecision);
+    }
+    
+    @Test
+    public void squareRootTest3() {
+        assertEquals( 0.0, am.squareRoot( 0 ), maxPrecision);
+    }
+    
+    @Test
+    public void squareRootTest4() {
+        assertEquals( 3145.23417252, am.squareRoot( 9892498 ), maxPrecision);
+    }
+    
+    @Test
+    public void squareRootTest5() {
+        assertEquals( Double.NaN, am.squareRoot( -4.0 ), maxPrecision);
+    }
+    
+    @Test
+    public void xSquareTest1() {
+        assertEquals( 1.0, am.xSquare( -1.0 ), maxPrecision);
+    }
+    
+    @Test
+    public void xSquareTest2() {
+        assertEquals( 0.0, am.xSquare( 0 ), maxPrecision);
+    }
+    
+    @Test
+    public void xSquareTest3() {
+        assertEquals( 67993974.60654682, am.xSquare( 8245.8459 ), maxPrecision);
+    }
+    
+    @Test
+    public void xSquareTest4() {
+        assertEquals( 30118144.0, am.xSquare( -5488 ), maxPrecision);
+    }
+    
+    @Test
+    public void xSquareTest5() {
+        assertEquals( 12345.65432100, am.xSquare( 111.111 ), maxPrecision);
+    }
+    
+    @Test
+    public void xCubeTest1() {
+        assertEquals( 0.0, am.xCube( 0 ), maxPrecision);
+    }
+    
+    @Test
+    public void xCubeTest2() {
+        assertEquals( -8.0, am.xCube( -2.0 ), maxPrecision);
+    }
+    @Test
+    public void xCubeTest3() {
+        assertEquals( 1371.70096063, am.xCube( 11.111 ), maxPrecision);
+    }
+    @Test
+    public void xCubeTest4() {
+        assertEquals( 146363183.0, am.xCube( 527 ), maxPrecision);
+    }
+    
+    @Test
+    public void xCubeTest5() {
+        assertEquals( -35643.77117099, am.xCube( -32.91 ), maxPrecision);
+    }
+    
+    @Test
+    public void xInverse1() {
+        assertEquals( Double.POSITIVE_INFINITY, am.xInverse( 0 ), maxPrecision);
+    }
+    
+    @Test
+    public void xInverse2() {
+        assertEquals( 0.5, am.xInverse(2), maxPrecision);
+    }
+    
+    @Test
+    public void xInverse3() {
+        assertEquals( 1.12019715, am.xInverse( 0.8927 ), maxPrecision);
+    }
+    
+    @Test
+    public void xInverse4() {
+        assertEquals( 0.30959752, am.xInverse( 3.23 ), maxPrecision);
+    }
+    
+    @Test
+    public void xInverse5() {
+        assertEquals( -0.000054127 , am.xInverse( -18474.84 ), maxPrecision);
+    }
+    
+    @Test
+    public void negateTest1() {
+        assertEquals( 18474.84 , am.negate( -18474.84 ), maxPrecision);
+    }
+    
+    @Test
+    public void negateTest2() {
+        assertEquals( -84 , am.negate( 84 ), maxPrecision);
+    }
+    
+    @Test
+    public void negateTest3() {
+        assertEquals( 0, am.negate( 0 ), maxPrecision);
+    }
+    
+    @Test
+    public void negateTest4() {
+        assertEquals( -1, am.negate(am.negate( am.negate( am.negate( -1 ) ) )), maxPrecision);
+    }
+    
+    @Test
+    public void xPowerYTest1() {
+        assertEquals( 16.0, am.xPowerY( 2, 4 ), maxPrecision);
+    }
+    
+    @Test
+    public void xPowerYTest2() {
+        assertEquals( 5927.15668503, am.xPowerY( 3.342, 7.2 ), maxPrecision);
+    }
+    
+    @Test
+    public void xPowerYTest3() {
+        assertEquals( 1.0, am.xPowerY( 234.34, 0 ), maxPrecision);
+    }
+    
+    @Test
+    public void xPowerYTest4() {
+        assertEquals( 0.0, am.xPowerY( 0, 34.8 ), maxPrecision);
+    }
+    
+    @Test
+    public void xPowerYTest5() {
+        assertEquals( 4294967296.0, am.xPowerY( 2, 32 ), maxPrecision);
+    }
+    
+    @Test
+    public void yRootXTest1() {
+        assertEquals("6.611489018457944", am.yRootX(289, 3));
+    }
+    
+    @Test
+    public void yRootXTest2() {
+        assertEquals("1.2437225540982386", am.yRootX(2823.4329, 36.43));
+    }
+    
+    @Test
+    public void yRootXTest3() {
+        assertEquals("1.3270621124806197", am.yRootX(890, 24));
+    }
+    
+    @Test
+    public void yRootXTest4() {
+        assertEquals("26.080460773387394", am.yRootX(9863, 2.82));
+    }
+    
+    @Test
+    public void yRootXTest5() {
+        assertEquals("2.0", am.yRootX(32, 5));
+    }
+    
+    @Test
+    public void yRootXTest6() {
+        assertEquals("NaN", am.yRootX(0, 0));
+    }
+    
+    @Test
+    public void yRootXTest7() {
+        assertEquals("infinity", am.yRootX(0, -5));
+    }
+    
+    @Test
+    public void yRootXTest8() {
+        assertEquals("0", am.yRootX(0, 3));
+    }
+    
+    @Test
+    public void yRootXTest9() {
+        assertEquals("infinity", am.yRootX(289, 0));
+    }
+    
+    @Test
+    public void yRootXTest10() {
+        assertEquals("NaN", am.yRootX(-8, 0));
+    }
+
+    @Test
+    public void yRootXTest11() {
+        assertEquals("1.565085 + i1.565085", am.yRootX(-24, 4));
+    }
+    
+    @Test
+    public void yRootXTest12() {
+        assertEquals("0.319472 - i0.319472", am.yRootX(-24, -4));
+    }
+    
+    @Test
+    public void yRootXTest13() {
+        assertEquals("0.45180100180492244", am.yRootX(24, -4));
+    }
+    
+    @Test
+    public void yRootXTest14() {
+        assertEquals("-1.888175022589804", am.yRootX(-24, 5));
+    }
+    
+    @Test
+    public void yRootXTest15() {
+        assertEquals("-0.5296119205244061", am.yRootX(-24, -5));
+    }
+    
+    @Test
+    public void yRootXTest16() {
+        assertEquals("0.5296119205244061", am.yRootX(24, -5));
+    }
+    
+    @Test
+    public void nFactorialTest1() {
+        assertEquals(120, am.nFactorial(5));
+    }
+    
+    @Test
+    public void nFactorialTest2() {
+        assertEquals(5040, am.nFactorial(7));
+    }
+    
+    @Test
+    public void nFactorialTest3() {
+        assertEquals(479001600, am.nFactorial(12));
+    }
+    
+    @Test
+    public void nFactorialTest4() {
+        assertEquals(1, am.nFactorial(0));
+    }
+    
+    @Test
+    public void nFactorialTest5() {
+        assertEquals(1, am.nFactorial(1));
+    }
+    
+}
