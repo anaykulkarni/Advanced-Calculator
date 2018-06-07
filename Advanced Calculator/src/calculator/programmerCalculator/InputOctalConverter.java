9fe31f9bf9b5113ceb097ac503e2ff2e9a2e0e5f
Kishore <35412658+useronezero@users.noreply.github.com>
2018-06-07 10:56:41 +0530

    Class to convert octal base value to different base values added.

---
+++ Advanced Calculator/src/calculator/programmerCalculator/InputOctalConverter.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator.programmerCalculator;
+
+/**
+ *
+ * @author kishore
+ */
+public class InputOctalConverter {
+    String convertOctalToDecimal(String inputOctValue) {
+        if("".equals(inputOctValue))
+            return "0";
+        else
+        {
+            int decimalEquivalent = Integer.parseInt(inputOctValue, 8);
+            return (Integer.toString(decimalEquivalent));
+        }
+        
+    }
+
+    String convertOctalToHexadecimal(String inputOctValue) {
+        int decimalConversion = Integer.parseInt(convertOctalToDecimal(inputOctValue));
+       return (Integer.toHexString(decimalConversion));
+    }
+    String convertOctalToBinary(String inputOctalValue) {
+        int decimalConversion = Integer.parseInt(convertOctalToDecimal(inputOctalValue));
+       return (Integer.toBinaryString(decimalConversion));
+    }
+}
